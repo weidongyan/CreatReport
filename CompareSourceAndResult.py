a2dea77cee68cf7c2b07a3c0a55650284d95e563
weidongyan <jackel.yan@qq.com>
2017-01-05 16:44:24 +0800

    bug fixed

--- CompareSourceAndResult.py
+++ CompareSourceAndResult.py
@@ -3,17 +3,17 @@ TicketIdCounter = 0
 UserIdCounter = 0
 CompareLog = file("CompareLog.txt",'a')
 for item in open("sourcefile.txt"):
-    #print len(item)
-    if len(item) ==13:
+    print len(item.strip())
+    if len(item.strip()) ==12:
         ProductIdCounter = ProductIdCounter +1
-    elif len(item) > 13:
+    elif len(item.strip()) > 12:
         TicketIdCounter = TicketIdCounter +1
-    elif len(item) == 4:
+    elif len(item.strip()) < 12:
         UserIdCounter = UserIdCounter +1
     else:
         print item
-        CompareLog.write("Error Find in Sourcefile \n")
-        CompareLog.write(item)
+        #CompareLog.write("Error Find in Sourcefile \n")
+        #CompareLog.write(item)
 print "ProductIdCounter is " + str(ProductIdCounter)
 print "TicketIdCounter is " +str(TicketIdCounter)
 print "UserIdCounter is " +str(UserIdCounter)
@@ -24,28 +24,34 @@ ProductIdSet = []
 ProductionIdInResult = 0
 for line in open("finalReport.txt"):
     RecordArray = line.strip().split(",")
-    if not RecordArray[3]:
-        CompareLog.write("Error find in finalReport: \n")
+    try:
+        if not RecordArray[3]:
+            CompareLog.write("Error find in finalReport: try \n")
+            CompareLog.write(line)
+    except:
         CompareLog.write(line)
+    ProductIdArray.append(RecordArray[0])
+    ProductIdSet = set(ProductIdArray)
     #print len(RecordArray)
     if len(RecordArray) ==4:
-        ProductIdArray.append(RecordArray[0])
-        ProductIdSet = set(ProductIdArray)
-        print RecordArray[3].strip()
+        #ProductIdArray.append(RecordArray[0])
+        #ProductIdSet = set(ProductIdArray)
+        #print RecordArray[3].strip()
         temp=RecordArray[3].strip()
         if temp.isdigit():
             #print "temp"
             #print temp
             ProductionIdInResult = ProductionIdInResult + int(temp)
     else:
-        print line
-        CompareLog.write("Error find in finalReport: \n")
+        #print line
+        CompareLog.write("Error find in finalReport:" + line +"\n")
 
 #print len(ProductIdArray)
 #print ProductIdSet
-print ProductionIdInResult
-print "Production Total in finalReport is: " +str(ProductionIdInResult)
+#print ProductionIdInResult
+#print "Production Total in finalReport is: " +str(ProductionIdInResult)
 CompareLog.write("Product Total in finalReport is: " + str(ProductionIdInResult) + '\n')
 CompareLog.write("Tickect Total in finalReport is: " + str(len(ProductIdSet)) +'\n')
+CompareLog.close()
 
 
